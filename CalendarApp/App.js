ff92ff9415b93f13c5c027e3270b30264c1fdfb6
jordanperu <jordan14@stanford.edu>
2023-05-10 11:18:30 -0700

    added basic layout for app

--- CalendarApp/App.js
+++ CalendarApp/App.js
@@ -1,14 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React, {useState, useEffect} from 'react';
+import {Text, View, ScrollView} from 'react-native';
+import RNCalendarEvents from 'react-native-calendar-events';
+
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+
+const App = () => {
+  const [events, setEvents] = useState([]);
+
+  const fetchCalendarEvents = async () => {
+    try {
+      const granted = await RNCalendarEvents.requestPermissions();
+
+      if (granted) {
+        const startDate = new Date();
+        const endDate = new Date(startDate);
+        endDate.setDate(startDate.getDate() + 30);
+
+        const fetchedEvents = await RNCalendarEvents.fetchAllEvents(
+          startDate.toISOString(),
+          endDate.toISOString(),
+        );
+
+        setEvents(fetchedEvents.slice(0, 3));
+      }
+    } catch (error) {
+      console.error('Error fetching calendar events:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCalendarEvents();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Upcoming Calendar Events:</Text>
+      <ScrollView>
+        {events.map((event, index) => (
+          <Text key={index}>{event.title}</Text>
+        ))}
+      </ScrollView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +64,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
